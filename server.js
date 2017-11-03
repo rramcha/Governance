fc31cd975ec854058e8178f3d34580990cecdd1a
Ravi <rramcha@users.noreply.github.com>
2017-11-03 16:50:45 -0500

    express routes and manage config properties

--- server.js
+++ server.js
@@ -1,15 +1,29 @@
 var express = require('express'),
-    bodyParser = require('body-parser'),
-    mongoose = require('mongoose'),
-    app = new express();
+bodyParser = require('body-parser'),
+mongoose = require('mongoose'),
+app = new express();
 
-app.use(express.static(__dirname + '/client'));
+//TO DO: config for setting up mongodb
+var db = require('./config/db.js');
 
+var port = process.env.PORT || 90; 
+
+//TO DO: set up logger
+
+//TO DO: set up nodemon - auto refresh
+
+//TO DO: set up taskrunner - GULP
+
+//TO DO: set up e2e testing - Protractor
+
+
+app.use('/crm', require('./app/routes'));
+app.use(express.static(__dirname + '/public'));
 app.use(bodyParser.json()); // to support JSON-encoded bodies
 app.use(bodyParser.urlencoded({ // to support URL-encoded bodies
-    extended: true
+	extended: true
 }));
+app.listen(port);
+console.log("App is running on-->" + port);
 
-
-app.listen(90);
-console.log("App is running on port number 90");
+exports = module.exports = app;
