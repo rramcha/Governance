1053258180127330d2c6f252c7e378ccd38182e3
DESKTOP-ECI0M9P\Ravi <ravindrar398@gmail.com>
2017-11-03 19:29:38 -0500

    configuring winston logger

--- server.js
+++ server.js
@@ -1,16 +1,41 @@
 var express = require('express'),
 bodyParser = require('body-parser'),
 mongoose = require('mongoose'),
-app = new express();
-
+app = new express(),
+winston = require('winston');
+winston.emitErrs = true;
 //TO DO: config for setting up mongodb
-var db = require('./config/db.js');
+//var db = require('./config/db.js');
 
 var port = process.env.PORT || 90; 
 
 //TO DO: set up logger
+var logger = new winston.Logger({
+    transports: [
+        new winston.transports.File({
+            level: 'info',
+            filename: './logs/all-logs.log',
+            handleExceptions: true,
+            json: true,
+            maxsize: 5242880, //5MB
+            maxFiles: 5,
+            colorize: false
+        }),
+        new winston.transports.Console({
+            level: 'debug',
+            handleExceptions: true,
+            json: false,
+            colorize: true
+        })
+    ],
+    exitOnError: false
+});
 
-//TO DO: set up nodemon - auto refresh
+module.exports.stream = {
+    write: function(message, encoding){
+        logger.info(message.slice(0, -1));
+    }
+};
 
 //TO DO: set up taskrunner - GULP
 
@@ -24,6 +49,7 @@ app.use(bodyParser.urlencoded({ // to support URL-encoded bodies
 	extended: true
 }));
 app.listen(port);
-console.log("App is running on-->" + port);
 
+console.log("App is running on-->" + port);
+module.exports = logger;
 exports = module.exports = app;
