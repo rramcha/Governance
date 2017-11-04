01d9fb21bfd9c5f7fada03f13d05a03549ed497b
DESKTOP-ECI0M9P\Ravi <ravindrar398@gmail.com>
2017-11-03 21:36:09 -0500

    logger fix

---
+++ logger.js
@@ -0,0 +1,37 @@
+/**
+ * @author rramcha
+ */
+
+var winston = require('winston');
+
+//winston.emitErrs = true;
+
+var logger = new winston.Logger({
+    transports: [
+        new winston.transports.File({
+            level: 'info',
+            filename: './logs/all-logs.log',
+            handleExceptions: true,
+            json: true,
+            maxsize: 5242880, 
+            maxFiles: 5,
+            colorize: false,
+            prettyPrint: true
+        }),
+        new winston.transports.Console({
+            level: 'debug',
+            handleExceptions: true,
+            json: false,
+            colorize: true
+        })
+    ],
+    exitOnError: false
+});
+
+module.exports.stream = {
+    write: function(message, encoding){
+        logger.info(message.slice(0, -1));
+    }
+};
+
+module.exports = logger;
