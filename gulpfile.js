cab19ca040a685b9b27652be970454c2e75efdad
DESKTOP-ECI0M9P\Ravi <ravindrar398@gmail.com>
2017-11-03 22:29:06 -0500

    gulp script to concat and minify all js

--- gulpfile.js
+++ gulpfile.js
@@ -1,7 +1,20 @@
+/**
+ * @author rramcha
+ */
+ 
 var gulp = require('gulp'),
-gutil = require('gulp-util');
+gutil = require('gulp-util'),
+uglify = require('gulp-uglify'),
+concat = require('gulp-concat');
 
-//TO DO: set up taskrunner - GULP
 gulp.task('default', function() {
   return gutil.log('Gulp is running!')
+});
+
+//concat and minify only js files
+gulp.task('js', function(){
+    return gulp.src('public/js/*.js')
+    .pipe(concat('all.js'))
+    .pipe(uglify())
+    .pipe(gulp.dest('dist/js/'))
 });
