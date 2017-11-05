a739b14ab32203b540967a468c6d5e19620da1cd
DESKTOP-ECI0M9P\Ravi <ravindrar398@gmail.com>
2017-11-05 01:28:47 -0500

    configuring app live reload

--- gulpfile.js
+++ gulpfile.js
@@ -5,16 +5,48 @@
 var gulp = require('gulp'),
 gutil = require('gulp-util'),
 uglify = require('gulp-uglify'),
-concat = require('gulp-concat');
+concat = require('gulp-concat'),
+jshint = require('gulp-jshint'),
+rename = require('gulp-rename'),
+minifycss = require('gulp-minify-css'),
+logger = require('./logger.js'),
+exec = require('child_process').exec,
+nodemon = require('gulp-nodemon');
 
-gulp.task('default', function() {
-  return gutil.log('Gulp is running!')
+// Lint Task
+gulp.task('lint', function() {
+    return gulp.src('public/js/*.js')
+        .pipe(jshint())
+        .pipe(jshint.reporter('default'));
 });
 
 //concat and minify only js files
-gulp.task('js', function(){
+gulp.task('scripts', function(){
     return gulp.src('public/js/*.js')
     .pipe(concat('all.js'))
     .pipe(uglify())
     .pipe(gulp.dest('dist/js/'))
 });
+
+// Watch Files For Changes
+gulp.task('watch', function() {
+    gulp.watch('public/js/*.js', ['lint', 'scripts']);
+    gulp.watch('public/css/*.css', ['css']);
+});
+
+// start the server and listen for changes
+gulp.task('server', function() {
+    // configure nodemon
+    nodemon({
+        script: 'server.js',
+        watch: ["server.js", 'public/*', 'public/*/**','app/*','app/*/**'],
+        ext: 'js'
+    }).on('restart', function() { 
+    
+    gulp.src('server.js')
+      .pipe(notify('Running the start tasks and stuff'));
+  });
+});
+
+// Default Task
+gulp.task('default', ['lint', 'scripts', 'watch','server']);
